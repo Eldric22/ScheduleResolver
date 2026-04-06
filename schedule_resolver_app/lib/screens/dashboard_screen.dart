5b17b7080a10a7b43b574628b25c59b3aa489f9d
Eldric22 <romanoeldric@gmail.com>
2026-04-06 09:27:29 +0000

    detect conflicts, rank priorities, and generate a revised schedule.

---
+++ schedule_resolver_app/lib/screens/dashboard_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/schedule_provider.dart';
+import '../services/ai_schedule_service.dart';
+import '../models/task_model.dart';
